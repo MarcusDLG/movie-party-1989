d63db6f78a120578099f4b260bca96cb037fd709
MarcusDLG <delag010@gmail.com>
2020-03-20 15:45:05 -0400

    initial build in place

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react'
+import Movie from './components/movie'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
+class App extends Component {
+  state = {
+    movieItems: [],
+    totalResults: 0,
+  }
+
+  componentDidMount() {
+    fetch()
+      .then(response => {
+        console.log('then-response' + response)
+        return response.json()
+      })
+      .then(data => {
+        console.log('then-data' + data)
+        this.setState({
+          movieItems: data.results,
+          totalResults: data.total_results,
+        })
+      })
+  }
+  render() {
+    return (
+      <>
+        <Movie
+          original_title={item.original_title}
+          poster_path={item.poster_path}
+          overview={item.overview}
+        />
+      </>
+    )
+  }
 }
 
 export default App
---
+++ src/components/Movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Movie extends Component {
+  render() {
+    return (
+      <section className="movieCard">
+        <head></head>
+      </section>
+    )
+  }
+}
+
+export default Movie
